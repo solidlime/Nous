db9192868b38d4f04ed63f5be9b10b4ff561168c
Raurau <raurau@solidlime.dev>
2026-07-12 01:28:27 +0900

    fix(config): ignore extra env vars in Settings (TZ breaking tests)

--- nous/config/settings.py
+++ nous/config/settings.py
@@ -200,6 +200,7 @@ class Settings(BaseSettings):
         env_nested_delimiter="__",
         env_file=".env",
         env_file_encoding="utf-8",
+        extra="ignore",
     )
 
     server: ServerConfig = ServerConfig()
